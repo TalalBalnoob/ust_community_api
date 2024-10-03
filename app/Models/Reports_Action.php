19c5ac2f167281114d71c30d3c9a0ee8b865a335
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2024-10-03 20:56:22 +0300

    add reports table

---
+++ app/Models/Reports_Action.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reports_Action extends Model {
+	protected $table = 'reports_actions';
+	use HasFactory;
+}
---
+++ database/migrations/2024_10_03_114405_create_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Reports_Action;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('reports_actions', function (Blueprint $table) {
+			$table->id();
+			$table->string('action');
+		});
+
+		Schema::create('reports', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('post_id')->references('id')->on('posts');
+			$table->foreignId('user_id')->references('id')->on('users');
+			$table->foreignId('reviewer_id')->nullable()->references('id')->on('users');
+			$table->text('report_text');
+			$table->boolean('isReviewed')->default(false);
+			$table->boolean('isValid')->nullable();
+			$table->foreignId('action')->nullable()->references('id')->on('reports_actions');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('reports');
+	}
+};
