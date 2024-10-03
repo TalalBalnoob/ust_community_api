7f3493c2743715a6c8a0324dab04567c5b3a28a2
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2024-10-03 22:03:44 +0300

    remove the home page with the blade file

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use App\Models\Major;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-	return view('welcome');
-});
-
-
-Route::get('/test', function () {
 	$major = Major::all()->random();
 	dd($major['years_of_study']);
 });
