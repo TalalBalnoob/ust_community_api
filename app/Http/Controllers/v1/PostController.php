c2bfdf1b7afd4eb42f050f486db284a9c9fc26ae
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2024-10-05 17:00:54 +0300

    add api versioning v1

--- app/Http/Controllers/v1/PostController.php
+++ app/Http/Controllers/v1/PostController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\v1;
 
+use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -10,13 +11,6 @@ class PostController extends Controller {
 	 * Display a listing of the resource.
 	 */
 	public function index() {
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function create() {
 		//
 	}
 
@@ -31,13 +25,6 @@ class PostController extends Controller {
 	 * Display the specified resource.
 	 */
 	public function show(Post $post) {
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 */
-	public function edit(Post $post) {
 		//
 	}
 
