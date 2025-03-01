d8fb5ec276d526670b4a2b2948607760475df6bc
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2025-03-01 14:40:31 +0300

    code

--- app/Http/Controllers/v1/AdminController.php
+++ app/Http/Controllers/v1/AdminController.php
@@ -9,70 +9,86 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
 
-class AdminController extends Controller {
-	public function getUserList(Request $request) {
-		return Response()->json(User::query()->get()->all());
-	}
+class AdminController extends Controller
+{
+    public function getUserList(Request $request)
+    {
+        return Response()->json(User::query()->get()->all());
+    }
 
-	public function searchUsers(Request $request) {
-		$searchText = $request['text'];
+    public function searchUsers(Request $request)
+    {
+        $searchText = $request['text'];
 
-		$users = Student::query()
-			->where('displayName', 'LIKE', "%{$searchText}%")
-			->get();
+        $users = Student::query()
+            ->where('displayName', 'LIKE', "%{$searchText}%")
+            ->get();
 
-		$users->merge(
-			Staff::query()
-				->where('displayName', 'LIKE', "%{$searchText}%")
-				->get()
-		);
+        $users->merge(
+            Staff::query()
+                ->where('displayName', 'LIKE', "%{$searchText}%")
+                ->get()
+        );
 
-		return Response()->json($users);
-	}
+        return Response()->json($users);
+    }
 
-	// TODO: Review the function
-	public function updateUser(Request $request, string $user_id) {
-		$user = User::query()->find($user_id);
-		if (!$user) {
-			abort(404, 'User Not found');
-		}
+    public function showUser(Request $request, string $user_id)
+    {
+        $user = User::query()->get()->where('id', $user_id)->first();
+
+        if ($user) {
+            abort('404', 'user not found');
+        }
+
+        return Response()->json($user);
+    }
+
+    // TODO: Review the function
+    public function updateUser(Request $request, string $user_id)
+    {
+        $user = User::query()->find($user_id);
+        if (!$user) {
+            abort(404, 'User Not found');
+        }
 
 
-		$validateReq = $request->validate(
-			[
-				'username' => ['string', 'min:5', 'max:12'],
-				'password' => [Password::min(8)],
-				'isAdmin' => ['boolean'],
-				'user_type_id' => ['between:1,2']
-			]
-		);
+        $validateReq = $request->validate(
+            [
+                'username' => ['string', 'min:5', 'max:12'],
+                'password' => [Password::min(8)],
+                'isAdmin' => ['boolean'],
+                'user_type_id' => ['between:1,2']
+            ]
+        );
 
-		if ($validateReq['username']) {
-			$user->username = $validateReq['username'];
-		}
-		if ($validateReq['password']) {
-			$user->password = $validateReq['password'];
-		}
-		if ($validateReq['isAdmin']) {
-			$user->isAdmin = $validateReq['isAdmin'];
-		}
-		// FIXME: need to make new profile and delete the old profile before change type of profile
-		if ($validateReq['user_type_id']) {
-			$user->user_type_id = $validateReq['user_type_id'];
-		}
+        if ($validateReq['username']) {
+            $user->username = $validateReq['username'];
+        }
+        if ($validateReq['password']) {
+            $user->password = $validateReq['password'];
+        }
+        if ($validateReq['isAdmin']) {
+            $user->isAdmin = $validateReq['isAdmin'];
+        }
+        // FIXME: need to make new profile and delete the old profile before change type of profile
+        if ($validateReq['user_type_id']) {
+            $user->user_type_id = $validateReq['user_type_id'];
+        }
 
-		$user->save();
+        $user->save();
 
-		return response()->json($user);
-	}
+        return response()->json($user);
+    }
 
-	public function deleteUser(Request $request) {
-		$targetUserId = $request['user_id'];
+    public function deleteUser(Request $request)
+    {
+        $targetUserId = $request['user_id'];
 
-		$targetUser = User::query()->find($targetUserId);
+        $targetUser = User::query()->find($targetUserId);
 
-		$targetUser ? $targetUser->delete() : abort(404, 'user not found');
+        $targetUser ? $targetUser->delete() : abort(404, 'user not found');
 
-		return response(['message' => 'user has been deleted']);
-	}
+        return response(['message' => 'user has been deleted']);
+    }
 }
--- app/Http/Controllers/v1/ReportController.php
+++ app/Http/Controllers/v1/ReportController.php
@@ -6,45 +6,53 @@ use App\Http\Controllers\Controller;
 use App\Models\Report;
 use Illuminate\Http\Request;
 
-class ReportController extends Controller {
-	public function index(Request $request) {
-		$isReviewed = $request['isReviewed'];
+class ReportController extends Controller
+{
+    public function index(Request $request)
+    {
+        $isReviewed = $request['isReviewed'];
 
-		if ($isReviewed === 'false') {
-			$reports = Report::query()->get()->where('isReviewed', 0);
-			return Response()->json($reports);
-		} elseif ($isReviewed === 'true') {
-			$reports = Report::query()->get()->where('isReviewed', 1);
-			return Response()->json($reports);
-		}
+        if ($isReviewed === 'false') {
+            $reports = Report::query()->get()->where('isReviewed', 0);
+            return Response()->json($reports);
+        } elseif ($isReviewed === 'true') {
+            $reports = Report::query()->get()->where('isReviewed', 1);
+            return Response()->json($reports);
+        }
 
-		$reports = Report::query()->get()->all();
+        $reports = Report::query()->get()->all();
 
-		return Response()->json($reports);
-	}
+        return Response()->json($reports);
+    }
 
-	public function show(Request $request, string $reportID) {
-		$report = Report::query()->find($reportID);
+    public function show(Request $request, string $reportID)
+    {
+        $report = Report::query()->find($reportID);
 
-		return Response()->json($report);
-	}
+        return Response()->json($report);
+    }
 
-	public function review(Request $request, string $reportID) {
-		$report = Report::query()->find($reportID);
+    public function review(Request $request, string $reportID)
+    {
+        $report = Report::query()->find($reportID);
 
-		if (!$report) abort(404, 'report not found');
-		if ($report->isReviewed) abort(400, 'Report has been reviewed');
+        if (!$report) {
+            abort(404, 'report not found');
+        }
+        if ($report->isReviewed) {
+            abort(400, 'Report has been reviewed');
+        }
 
-		$report->isReviewed = true;
-		$report->reviewer_id = $request->user()->id;
+        $report->isReviewed = true;
+        $report->reviewer_id = $request->user()->id;
 
-		$report->update(
-			[
-				"isReviewed" => true,
-				"reviewer_id" => $request->user()->id
-			]
-		);
+        $report->update(
+            [
+                "isReviewed" => true,
+                "reviewer_id" => $request->user()->id
+            ]
+        );
 
-		return Response()->json('report is reviewed now');
-	}
+        return Response()->json('report is reviewed now');
+    }
 }
